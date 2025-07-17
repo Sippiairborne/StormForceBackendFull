d277081be24881fbbc849e9c952bb6f9a50b8515
Sippiairborne <claimsmasters0@gmail.com>
2025-07-17 11:16:38 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,22 @@
+// functions/index.js
+
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
+const claimsRoutes = require("./routes/claims");
+const stripeController = require("./controllers/stripeController");
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/claims", claimsRoutes);
+app.use("/api/stripe", stripeController);
+
+// Export Firebase Function
+exports.api = functions.https.onRequest(app);
 // Firebase Cloud Function entry point
